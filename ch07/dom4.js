05189caacc2436d5480738b8bc852164942ecc9d
Tessa <kwonyuj1996@gmail.com>
2022-05-28 19:32:14 +0900

    first commit - adding dataInput function

--- ch07/dom4.js
+++ ch07/dom4.js
@@ -19,27 +19,33 @@ function formPrac() {
     phone: '010-5555-6666',
     age: 20}];
 
-    for (let member of members) {
-      let tr = document.createElement('tr');
-      let td, btn, chk;  
-      td = document.createElement('td');
-      chk = document.createElement('input');
-      chk.setAttribute('type', 'checkbox');
-      td.append(chk);
-      tr.append(td);
-      for (let field in member) {
+    function dataInput(ary = []) {
+      let tr, td, btn, chk;
+      ary.forEach((elem, idx) => {
+        tr = document.createElement('tr');
+        console.log(tr);
         td = document.createElement('td');
-        td.innerText = member[field];
+        chk = document.createElement('input');
+        chk.setAttribute('type', 'checkbox');
+        td.append(chk);
         tr.append(td);
-      }
-      td = document.createElement('td');
-      btn = document.createElement('button');
-      btn.innerText = "삭제";
-      td.append(btn);
-      tr.append(td);
-      let tbody = document.querySelector('#list');
-      tbody.append(tr);
+        for (let field in elem) {
+          td = document.createElement('td');
+          td.innerText = elem[field];
+          tr.append(td);
+        }
+        td = document.createElement('td');
+        btn = document.createElement('button');
+        btn.innerText = "삭제";
+        btn.addEventListener('click', (e) => e.target.parentElement.parentElement.remove())
+        td.append(btn);
+        tr.append(td);
+        document.querySelector('#list').append(tr);
+      })
     }
+    dataInput(members);
+
+
  
 
   // 선택 삭제
@@ -56,57 +62,62 @@ function formPrac() {
   document.forms.frm.onsubmit = function(ev) {
     ev.preventDefault();
     // 사용자 입력값
-    let fn = this.fname.value;
-    let ln = this.lname.value;
+    let first_name = this.fname.value;
+    let last_name = this.lname.value;
     let phone = this.phone.value;
     let age = this.age.value;
-    if (!fn || !ln || !phone || !age) {
+    if (!first_name || !last_name || !phone || !age) {
       return alert("값을 입력해주세요");
+    } else {
+      let inputAry = [{first_name, last_name, phone, age}];
+      dataInput(inputAry);
+      console.log(this);
+      this.fname.value = '';
+      this.lname.value = '';
+      this.phone.value = '';
+      this.age.value = '';
     }
     // document.querySelectorAll('form>input').forEach(elem => {
     //   if (elem.type == 'text' || elem.type == 'number' || elem.type == 'tel') {
     //     inValues.push(elem.value);
     //   }
     // })
-    let inputAry = [fn, ln, phone, age];
+    
     
     // 태그
-    let trTag = document.createElement('tr');
-    let tdTag;
-    let chk; 
-    let btn;
-    inputAry.forEach((el, idx) => {
-      if (idx == 0) {
-        tdTag = document.createElement('td');
-        chk = document.createElement('input');
-        chk.setAttribute('type', 'checkbox');
-        tdTag.append(chk);
-        trTag.append(tdTag);
-      }
-      tdTag = document.createElement('td');
-      tdTag.innerText = el;
-      trTag.append(tdTag);
-      if (idx == inputAry.length - 1) {
-        tdTag = document.createElement('td');
-        btn = document.createElement('button');
-        // btn.addEventListener('click', deleteInfo);
-        // function deleteInfo() {
-        //   this.parentElement.parentElement.remove();
-        // }
+    // let trTag = document.createElement('tr');
+    // let tdTag;
+    // let chk; 
+    // let btn;
+    // inputAry.forEach((el, idx) => {
+    //   if (idx == 0) {
+    //     tdTag = document.createElement('td');
+    //     chk = document.createElement('input');
+    //     chk.setAttribute('type', 'checkbox');
+    //     tdTag.append(chk);
+    //     trTag.append(tdTag);
+    //   }
+    //   tdTag = document.createElement('td');
+    //   tdTag.innerText = el;
+    //   trTag.append(tdTag);
+    //   if (idx == inputAry.length - 1) {
+    //     tdTag = document.createElement('td');
+    //     btn = document.createElement('button');
+    //     // btn.addEventListener('click', deleteInfo);
+    //     // function deleteInfo() {
+    //     //   this.parentElement.parentElement.remove();
+    //     // }
 
-        // 화살표 함수를 쓸 경우에는 매개값으로 이벤트 안 넘겨주면 전체 다 삭제 (윈도우로 넘어감)
-         // console.log(e.target); // 이벤트의 target = <button>삭제</button>
-        btn.addEventListener('click', (e) => e.target.parentElement.parentElement.remove())
-        btn.innerText = '삭제';
-        tdTag.append(btn);
-        trTag.append(tdTag);
-      }
-    })
-    document.getElementById('list').append(trTag);
-    this.fname.value = '';
-    this.lname.value = '';
-    this.phone.value = '';
-    this.age.value = '';
+    //     // 화살표 함수를 쓸 경우에는 매개값으로 이벤트 안 넘겨주면 전체 다 삭제 (윈도우로 넘어감)
+    //      // console.log(e.target); // 이벤트의 target = <button>삭제</button>
+    //     btn.addEventListener('click', (e) => e.target.parentElement.parentElement.remove())
+    //     btn.innerText = '삭제';
+    //     tdTag.append(btn);
+    //     trTag.append(tdTag);
+    //   }
+    // })
+    // document.getElementById('list').append(trTag);
+    
   }
 }
 
