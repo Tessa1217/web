576025b8c080341f553d79ab83f4a2b02012e1bd
Tessa <kwonyuj1996@gmail.com>
2022-05-31 15:27:10 +0900

    first commit - ajax web project practice

---
+++ Ajax/src/main/java/co/edu/AjaxServlet.java
@@ -0,0 +1,107 @@
+package co.edu;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+/**
+ * Servlet implementation class AjaxServlet
+ */
+@WebServlet({ "/AjaxServlet", "/ajax.do" })
+public class AjaxServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public AjaxServlet() {
+		super();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException { // response 결과를 가지고 있는 객체
+		response.setCharacterEncoding("utf-8"); // utf-8 인코딩
+		response.setContentType("text/html;charset=utf-8"); // 컨텐트 타입은 html
+		PrintWriter out = response.getWriter(); // 출력 스트림
+		String job = request.getParameter("job"); // 요청 정보에서 job이라는 파라미터를 읽어옴
+		if (job.equals("html")) {
+			out.print("<h3>아작스페이지 입니다.</h3>"); // 위에서 컨텐트 타입과 인코딩 타입을 지정해주지 않으면 기본적으로 text
+			out.print("<a href = 'index.html'>메인 페이지</a>");
+		} else if (job.equals("json")) {
+			// JSON 형식 데이터 [{"fname":?}, {"lname":?}, {"email":?} ...]
+			EmpDAO dao = new EmpDAO();
+			List<Employee> eList = dao.empList();
+//			String json = "[";
+//			for (int i = 0; i < eList.size(); i++) {
+//				json += "{\"fname\":" + eList.get(i).getFirstName() + "}";
+//				if (i != eList.size() - 1) {
+//					json += ',';
+//				}
+//			}
+//			json += "]";
+//			out.print(json);
+			Gson gson = new GsonBuilder().create();
+			out.print(gson.toJson(eList));
+		}
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// contextPath - 프로젝트 제일 상위 정보 (served at: /Ajax)
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=utf-8");
+		String cmd = request.getParameter("cmd");
+		String empId = request.getParameter("empId");
+		String fname = request.getParameter("fname");
+		String lname = request.getParameter("lname");
+		String email = request.getParameter("email");
+		String hdate = request.getParameter("hdate");
+		String job = request.getParameter("job");
+
+		Employee emp = new Employee();
+		emp.setEmployeeId(Integer.parseInt(empId));
+		emp.setFirstName(fname);
+		emp.setLastName(lname);
+		emp.setEmail(email);
+		emp.setHireDate(hdate);
+		emp.setJobId(job);
+
+		EmpDAO dao = new EmpDAO();
+
+		if (cmd.equals("insert")) {
+			dao.insert(emp);
+			System.out.println(emp);
+		} else if (cmd.equals("update")) {
+			if (dao.update(emp) == null) {
+				// {"retCode":"error"}
+				System.out.println("error");
+			} else {
+				// {"retCode":"success"}
+				System.out.println("success");
+			}
+		}
+		Gson gson = new GsonBuilder().create();
+		response.getWriter().print(gson.toJson(emp));
+	}
+
+}
---
+++ Ajax/src/main/java/co/edu/EmpDAO.java
@@ -0,0 +1,122 @@
+package co.edu;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmpDAO extends DAO {
+
+	// 리스트 가져오기
+	public List<Employee> empList() {
+		String sql = "SELECT * FROM EMP_TEMP";
+		List<Employee> eList = new ArrayList<Employee>();
+		connect();
+		try {
+			psmt = con.prepareStatement(sql);
+			rs = psmt.executeQuery();
+			while (rs.next()) {
+				Employee emp = new Employee();
+				emp.setEmployeeId(rs.getInt("employee_id"));
+				emp.setFirstName(rs.getString("first_name"));
+				emp.setLastName(rs.getString("last_name"));
+				emp.setEmail(rs.getString("email"));
+				emp.setJobId(rs.getString("job_id"));
+				emp.setHireDate(rs.getString("hire_date"));
+				eList.add(emp);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			disconnect();
+		}
+		return eList;
+	}
+
+	// 입력
+	public Employee insert(Employee emp) {
+		int insert = -1;
+		String sql = "INSERT INTO EMP_TEMP(EMPLOYEE_ID, FIRST_NAME, LAST_NAME, EMAIL, JOB_ID, HIRE_DATE) "
+				+ "VALUES (?, ?, ?, ?, ?, ?)";
+		String seqSql = "SELECT EMPLOYEES_SEQ.NEXTVAL FROM DUAL";
+		connect();
+		int nextSeq = -1;
+		try {
+			psmt = con.prepareStatement(seqSql);
+			rs = psmt.executeQuery();
+			if (rs.next()) {
+				nextSeq = rs.getInt(1);
+			}
+			psmt = con.prepareStatement(sql);
+			psmt.setInt(1, nextSeq);
+			psmt.setString(2, emp.getFirstName());
+			psmt.setString(3, emp.getLastName());
+			psmt.setString(4, emp.getEmail());
+			psmt.setString(5, emp.getJobId());
+			psmt.setString(6, emp.getHireDate());
+			insert = psmt.executeUpdate();
+			System.out.println(insert + "건이 입력되었습니다.");
+			emp.setEmployeeId(nextSeq);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			disconnect();
+		}
+		return emp;
+	}
+
+	// 수정
+	public Employee update(Employee emp) {
+		int update = -1;
+		String sql = "UPDATE EMP_TEMP SET FIRST_NAME = ?, " + "LAST_NAME = ?, EMAIL = ?, HIRE_DATE = ? WHERE "
+				+ "EMPLOYEE_ID = ?";
+		connect();
+		try {
+			psmt = con.prepareStatement(sql);
+			psmt.setString(1, emp.getFirstName());
+			psmt.setString(2, emp.getLastName());
+			psmt.setString(3, emp.getEmail());
+			psmt.setString(4, emp.getHireDate());
+			psmt.setInt(5, emp.getEmployeeId());
+			update = psmt.executeUpdate();
+			if (update > 0) {
+				System.out.println(update + "건 수정되었습니다.");
+				return emp;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			disconnect();
+		}
+		return null;
+	}
+//
+//	// 삭제
+//	public int delete() {
+//		int delete = -1;
+//		String sql = "DELETE FROM EMP_TEMP WHERE ";
+//		connect();
+//		try {
+//			psmt = con.prepareStatement(sql);
+//			delete = psmt.executeUpdate();
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		} finally {
+//			disconnect();
+//		}
+//		return delete;
+//	}
+//
+//	// 한 건 조회
+//	public Employee search() {
+//		String sql = "";
+//		connect();
+//		try {
+//			psmt = con.prepareStatement(sql);
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		} finally {
+//			disconnect();
+//		}
+//	}
+
+}
